a97b660995a63e823296a436117a12c3c0bb3f34
Roneryo <roner.ortega@gmail.com>
2017-06-27 10:23:49 -0400

    Update views.py

--- Unegweb/Estudiante/views.py
+++ Unegweb/Estudiante/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .forms import MiFormulario
 # Create your views here.
+
+def Registro(request):
+	form=MiFormulario()
+	context={
+		"form":form
+	}
+	return render(request,"registro.html",context)
