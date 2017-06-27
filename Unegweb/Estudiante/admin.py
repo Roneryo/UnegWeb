e9d7c82d2e4af4718123ea8305d349fd5b56bd47
Roneryo <roner.ortega@gmail.com>
2017-06-27 10:19:08 -0400

    Update admin.py

--- Unegweb/Estudiante/admin.py
+++ Unegweb/Estudiante/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from __future__ import unicode_literals
 
-# Register your models here.
+from django.db import models
+# Create your models here.
+class Estudiante(models.Model):
+	nombre=models.CharField(max_length=20)
+	apellido=models.CharField(max_length=20)
+	edad=models.IntegerField()
+	
