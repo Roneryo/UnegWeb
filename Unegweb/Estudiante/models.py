c38a96a0ed8a282dfb89e78ab86250f634e04124
Roneryo <roner.ortega@gmail.com>
2017-06-27 10:22:30 -0400

    Update models.py

--- Unegweb/Estudiante/models.py
+++ Unegweb/Estudiante/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
 # Create your models here.
+class Estudiante(models.Model):
+	nombre=models.CharField(max_length=20)
+	apellido=models.CharField(max_length=20)
+	edad=models.IntegerField()
+	
